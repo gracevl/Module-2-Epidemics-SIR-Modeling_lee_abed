cd352b7e876b0b24eebe8b561c763aefa4c3c7c9
gracevl <qjj5rc@virginia.edu>
2026-02-26 13:32:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import pandas as pd
 
 data = pd.read_csv("/Users/zain/Documents/GitHub/Module-2-Epidemics-SIR-Modeling_lee_abed/Data/mystery_virus_daily_active_counts_RELEASE#1.csv")
 
-plt.scatter(data["day"], data["active reported daily cases"]) #create scatter of days vs. reported cases 
-plt.xlabel("Day") # labeling the x value 
-plt.ylabel("Active Reported Daily Cases") # labeling the y value 
+plt.plot(data["day"], data["active reported daily cases"])
+plt.xlabel("Day")
+plt.ylabel("Active Reported Daily Cases")
 plt.title("Day vs. Active Reported Daily Cases")
 plt.show() 
 
