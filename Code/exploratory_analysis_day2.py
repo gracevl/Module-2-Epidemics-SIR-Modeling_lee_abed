d5778b53407bd5057ec5edb0ef5ee580562189eb
gracevl <qjj5rc@virginia.edu>
2026-02-26 18:00:13 -0500

    Update exploratory_analysis_day2.py

--- Code/exploratory_analysis_day2.py
+++ Code/exploratory_analysis_day2.py
@@ -5,12 +5,21 @@ import numpy as np
 from scipy.optimize import curve_fit
 #%%
 # Load the data
-data = pd.read_csv('../Data/mystery_virus_daily_active_counts_RELEASE#1.csv', parse_dates=['date'], header=0, index_col=None)
+data = pd.read_csv("/Users/gracelee/Documents/computational BME/Module-2-Epidemics-SIR-Modeling_lee_abed/Data/mystery_virus_daily_active_counts_RELEASE#1.csv", parse_dates=['date'], header=0, index_col=None)
 #%%
 # We have day number, date, and active cases. We can use the day number and active cases to fit an exponential growth curve to estimate R0.
 # Let's define the exponential growth function
 def exponential_growth(t, r):
     return np.exp(r * t)
+
+data = pd.read_csv("/Users/gracelee/Documents/computational BME/Module-2-Epidemics-SIR-Modeling_lee_abed/Data/mystery_virus_daily_active_counts_RELEASE#1.csv")
+
+plt.scatter(data["day"], data["active reported daily cases"])
+plt.xlabel("Day")
+plt.ylabel("Active Reported Daily Cases")
+plt.title("Day vs. Active Reported Daily Cases")
+plt.plot(exponential_growth(data["day"], data["active reported daily cases"]))
+plt.show() 
 
 
 # Fit the exponential growth model to the data. 
