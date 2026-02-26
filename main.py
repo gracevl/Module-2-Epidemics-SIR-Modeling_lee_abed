f1efecc4dff64637009a37fd9f8081d2ee5b1d57
Zain Abed <zain.abed311@gmail.com>
2026-02-26 13:10:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-data = pd.read_csv("/Users/gracelee/Documents/computational BME/Module-2-Epidemics-SIR-Modeling_lee_abed/Data/mystery_virus_daily_active_counts_RELEASE#1.csv")
+data = pd.read_csv("/Users/zain/Documents/GitHub/Module-2-Epidemics-SIR-Modeling_lee_abed/Data/mystery_virus_daily_active_counts_RELEASE#1.csv")
 
-plt.plot(data["day"], data["active reported daily cases"])
+plt.scatter(data["day"], data["active reported daily cases"]) 
 plt.xlabel("Day")
 plt.ylabel("Active Reported Daily Cases")
 plt.title("Day vs. Active Reported Daily Cases")
