ee48bc2224e1dbf48aea5a196f974f7f355330e9
gracevl <qjj5rc@virginia.edu>
2026-02-27 09:44:15 -0500

    Update exploratory_analysis_day2.py

--- Code/exploratory_analysis_day2.py
+++ Code/exploratory_analysis_day2.py
@@ -12,20 +12,26 @@ data = pd.read_csv("/Users/gracelee/Documents/computational BME/Module-2-Epidemi
 def exponential_growth(t, r):
     return np.exp(r * t)
 
-data = pd.read_csv("/Users/gracelee/Documents/computational BME/Module-2-Epidemics-SIR-Modeling_lee_abed/Data/mystery_virus_daily_active_counts_RELEASE#1.csv")
-
-plt.scatter(data["day"], data["active reported daily cases"])
-plt.xlabel("Day")
-plt.ylabel("Active Reported Daily Cases")
-plt.title("Day vs. Active Reported Daily Cases")
-plt.plot(exponential_growth(data["day"], data["active reported daily cases"]))
-plt.show() 
-
 
 # Fit the exponential growth model to the data. 
 # We'll use a handy function from scipy called CURVE_FIT that allows us to fit any given function to our data. 
 # We will fit the exponential growth function to the active cases data. HINT: Look up the documentation for curve_fit to see how to use it.
 
+x = data["day"]
+y = data["active reported daily cases"]
+
+param, covariance = curve_fit(exponential_growth, x, y)
+
+plt.scatter(data["day"], data["active reported daily cases"])
+plt.xlabel("Day")
+plt.ylabel("Active Reported Daily Cases")
+plt.title("Day vs. Active Reported Daily Cases")
+
 # Approximate R0 using this fit
+r0 = np.exp(param * 2)  # Our infectious period is 2 days. R0 should be e^(r * infectious_period)
+print ("Estimated R0:", r0)
 
 # Add the fit as a line on top of your scatterplot.
+plt.plot(x, exponential_growth(x, *param), label='Fitted Exponential Growth Curve')
+plt.legend()
+plt.show() 
